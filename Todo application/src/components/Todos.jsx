bd15e63ad5dd5e0964944809231de89fa094d315
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2024-05-22 00:10:51 +0530

    Add files via upload

---
+++ Todo application/src/components/Todos.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+
+function Todos() {
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+
+  return (
+    <>
+      <div>Todos</div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
+        </li>
+      ))}
+    </>
+  )
+}
+
+export default Todos
